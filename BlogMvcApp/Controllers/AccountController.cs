635a4d87390b3a1e6f826a419cf5f8c696936747
Berkay Uzumcu <berkayuzumcu15@gmail.com>
2020-04-20 19:34:37 +0300

    Add project files.

---
+++ BlogMvcApp/Controllers/AccountController.cs
@@ -0,0 +1,122 @@
+﻿using BlogMvcApp.Models;
+using BlogMvcApp.NewFolder1;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogMvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        // GET: Account
+
+        public AccountController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new IdentityDataContext()));
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel loginModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var user = userManager.Find(loginModel.UserName, loginModel.Password);
+
+               
+
+                if (user == null)
+                {
+
+                    ModelState.AddModelError(" ", "Yanlış kullanıcı adı veya parola ama hangisi söylemem :)");
+                }
+                else
+                {
+
+                    var authManager = HttpContext.GetOwinContext().Authentication;
+                    var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+                    var authProperties = new AuthenticationProperties()
+                    {
+
+                        IsPersistent = true
+                    };
+
+                    authManager.SignOut();
+                    authManager.SignIn(authProperties, identity);
+                    return Redirect(string.IsNullOrEmpty(returnUrl)?"/" : returnUrl);
+                }
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View(loginModel);
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+
+                var result = userManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(" ", item);
+
+                    }
+                }
+
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
+
+
+
+
---
+++ BlogMvcApp/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using BlogMvcApp.NewFolder1;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogMvcApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        private UserManager<ApplicationUser> userManager;
+        // GET: Account
+
+        public AdminController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new IdentityDataContext()));
+
+        }
+        public ActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
